9ad8cee7f21523e071a5520772754e6ea3f2cec7
DESKTOP-DLBS2HQ\praktikant <kristiyankyukov@abv.bg>
2017-09-29 12:35:27 +0200

    Made file open dialog and currently working on xlf reading

---
+++ XLFezEditor/Wpfapp1/Files/ContextGroup.cs
@@ -0,0 +1,16 @@
+﻿namespace XLFezEditor.Files
+{
+    public class ContextGroup
+    {
+
+        public string source { get; set; }
+        public string lineNumber { get; set; }
+
+        public ContextGroup(string source, string lineNumber)
+        {
+            this.source = source;
+            this.lineNumber = lineNumber;
+
+        }
+    }
+}
